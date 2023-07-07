137a787ddd8ee36a89e6f02b0756a56773f84afe
Raleigh Littles III <raleighlittles@gmail.com>
2023-07-07 01:25:07 -0700

    format, cleanup

--- config_generator/canedge_config_generator.py
+++ config_generator/canedge_config_generator.py
@@ -17,8 +17,32 @@ PID_RESPONSE_DATA_FILE = "pid_responses.txt"
 CANEDGE_CANBUS_TRANSMIT_REQ_TYPE = "7DF"
 
 
-def generate_canedge_config(pid_names: typing.List, pid_periods: typing.List, pid_delays: typing.List, pid_response_data: typing.List):
-    pass
+def generate_canedge_config(pid_names: typing.List, pid_periods: typing.List, pid_delays: typing.List, pid_response_data: typing.List) -> dict:
+    transmit_blocks = dict()
+    transmit_blocks["transmit"] = list()
+
+    for idx in range(0, len(pid_names)):
+
+        pid_transmission_obj = dict()
+
+        # Most fields are hardcoded from the config. Make sure to load this into the Canedge config
+        # tester to verify, before use: https://canlogger.csselectronics.com/simple-editor/
+
+        pid_transmission_obj["name"] = pid_names[idx]
+        pid_transmission_obj["state"] = 1
+        pid_transmission_obj["id_format"] = 0
+        pid_transmission_obj["frame_format"] = 0
+        pid_transmission_obj["brs"] = 0
+        # controls whether the CAN frame itself gets logged or not (?)
+        pid_transmission_obj["log"] = 1
+        pid_transmission_obj["period"] = int(pid_periods[idx])
+        pid_transmission_obj["delay"] = int(pid_delays[idx])
+        pid_transmission_obj["id"] = CANEDGE_CANBUS_TRANSMIT_REQ_TYPE
+        pid_transmission_obj["data"] = pid_response_data[idx]
+
+        transmit_blocks["transmit"].append(pid_transmission_obj)
+
+    return transmit_blocks
 
 
 def get_lines_from_file(filename: str) -> typing.List:
@@ -44,28 +68,7 @@ if __name__ == "__main__":
         sys.exit(3)
 
     else:
-        transmit_blocks = dict()
-        transmit_blocks["transmit"] = list()
-
-        for idx in range(0, len(pid_names)):
-
-            pid_transmission_obj = dict()
-
-            # Most fields are hardcoded from the config. Make sure to load this into the Canedge config
-            # tester to verify, before use: https://canlogger.csselectronics.com/simple-editor/
-
-            pid_transmission_obj["name"] = pid_names[idx]
-            pid_transmission_obj["state"] = 1
-            pid_transmission_obj["id_format"] = 0
-            pid_transmission_obj["frame_format"] = 0
-            pid_transmission_obj["brs"] = 0
-            # controls whether the CAN frame itself gets logged or not (?)
-            pid_transmission_obj["log"] = 1
-            pid_transmission_obj["period"] = int(pid_periods[idx])
-            pid_transmission_obj["delay"] = int(pid_delays[idx])
-            pid_transmission_obj["id"] = CANEDGE_CANBUS_TRANSMIT_REQ_TYPE
-            pid_transmission_obj["data"] = pid_response_data[idx]
-
-            transmit_blocks["transmit"].append(pid_transmission_obj)
-
-    print(json.dumps(transmit_blocks))
+        # Doesn't look like there's a good way to easily prettyprint/format the JSON
+        # https://stackoverflow.com/questions/12943819/
+        print(json.dumps(generate_canedge_config(
+            pid_names, pid_periods, pid_delays, pid_response_data)))
